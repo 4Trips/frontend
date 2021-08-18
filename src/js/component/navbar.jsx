d9281221fd765ea6f47d4e74f0e6acdcb15da370
4Trips <4tripsfor@gmail.com>
2021-08-18 16:38:57 +0000

    navbar con estructura

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -3,6 +3,9 @@ import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import PropTypes from "prop-types";
@@ -12,24 +15,33 @@ const Menu = props => {
 	return (
 		<Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
 			<Container>
-				<Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+				<Navbar.Brand href="/">
+					<img src={logotipo} className="logo" />
+				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="responsive-navbar-nav" />
 				<Navbar.Collapse id="responsive-navbar-nav">
 					<Nav className="me-auto">
-						<Nav.Link href="#features">Features</Nav.Link>
-						<Nav.Link href="#pricing">Pricing</Nav.Link>
-						<NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-							<NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-							<NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-							<NavDropdown.Divider />
-							<NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-						</NavDropdown>
+						<Nav.Link href="#pricing">¿Qué es 4Trips?</Nav.Link>
+						<Nav.Link href="#pricing">¿Cómo publicar un viaje?</Nav.Link>
+					</Nav>
+					<Nav className="me-auto">
+						<Form className="d-flex">
+							<FormControl
+								type="search"
+								placeholder="Buscar viajes"
+								className="mr-2"
+								aria-label="Search"
+							/>
+							<Button variant="outline-light">Buscar</Button>
+						</Form>
 					</Nav>
 					<Nav>
-						<Nav.Link href="#deets">More deets</Nav.Link>
+						<NavDropdown title="Registro" id="collasible-nav-dropdown">
+							<NavDropdown.Item href="#action/3.1">Viajero</NavDropdown.Item>
+							<NavDropdown.Item href="#action/3.2">Profesional</NavDropdown.Item>
+						</NavDropdown>
 						<Nav.Link eventKey={2} href="#memes">
-							Dank memes
+							Iniciar sesión
 						</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
