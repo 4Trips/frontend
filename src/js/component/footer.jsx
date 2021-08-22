e65410fbd083d5c186a36e029190c243e4b91ced
4Trips <4tripsfor@gmail.com>
2021-08-22 12:22:52 +0000

    footer

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,10 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Nav from "react-bootstrap/Nav";
+import "../../styles/footer.scss";
+import facebook from "../../img/facebook.png";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			<a href="#">Política de privacidad</a>
-			<a href="#">Contactar</a>
-		</p>
-	</footer>
+	<div className="footer">
+		<div className="d-flex flex-row-reverse">
+			<Nav.Item>
+				<img src={facebook} />
+			</Nav.Item>
+			<Nav.Item>
+				<Nav.Link href="#" className="text-muted">
+					Contactar
+				</Nav.Link>
+			</Nav.Item>
+			<Nav.Item>
+				<Nav.Link href="#" className="text-muted">
+					Privacidad
+				</Nav.Link>
+			</Nav.Item>
+		</div>
+		<Card.Footer className="text-muted">
+			{" "}
+			&copy; {new Date().getFullYear()} Copyright: <a href="https://www.4trips.com"> 4 Trips</a>
+		</Card.Footer>
+	</div>
 );
