1d3da6c5ceb342e7dd9f6e5514fe23d64e35b2d2
4Trips <4tripsfor@gmail.com>
2021-12-27 22:37:15 +0000

    button publicar

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,11 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 import { TripCard } from "../component/tripCard.jsx";
 
 export const Home = () => (
-	<div className="mt-3">
-		<h1>Últimos viajes</h1>
+	<div className="container-fluid">
+		<div className="col">
+			<h3>Últimos viajes publicados:</h3>
+		</div>
+		<div className="col-md-3 offset-md-9">
+			<Link className="btn btn-dark" to="/publicarViaje">
+				Publicar viaje
+			</Link>
+		</div>
 		<TripCard />
 	</div>
 );
